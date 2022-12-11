d96820cf5cd7bf1887e5f7f351f46f68402ec0d6
pripat-2002 <77854263+pripat-2002@users.noreply.github.com>
2022-12-12 01:32:56 +0530

    updates to pychael.py
    Added a progress bar with animation for showing download progress.
    Yet to add user input for how many episodes to download in one run.

--- pychael_dl.py
+++ pychael_dl.py
@@ -1,6 +1,8 @@
-import requests
 import os
+import re
+import requests
 from bs4 import BeautifulSoup
+from clint.textui import progress
 
 '''
 URL of the archive web-page which provides link to
@@ -10,7 +12,7 @@ In this example, we first crawl the webpage to extract
 all the links and then download videos.
 '''
 
-def get_video_links(archive_url):
+def get_video_dic(archive_url):
 	
 	# create response object
 	r = requests.get(archive_url)
@@ -24,10 +26,30 @@ def get_video_links(archive_url):
 	# filter the link sending with .mp4
 	video_links = [archive_url + link['href'] for link in links if link['href'].endswith('mp4')]
 
-	return video_links
+	# get video sizes
+	video_sizes = []
+	text = soup.get_text()
+	text = text.split('\n')
+	for elem in text:
+		s = re.findall(f'\d+$',elem)
+		if s:
+			video_sizes.append(s[0])
+
+	if len(video_links)!=len(video_sizes):
+		print("\n\nFATAL ERROR: number of video links and video sizes mismatch! \nCode execution is terminated.")
+		exit()
+
+	# create a dictionary of video link and video size
+	video_dic = {}
+	for i in range(len(video_links)):
+		video_dic[video_links[i]]=video_sizes[i]
+
+	return video_dic
 
 
-def download_video_series(video_links, save_path):
+def download_video_series(video_dic, save_path):
+
+	video_links = list(video_dic.keys())
 
 	for link in video_links:
 
@@ -37,12 +59,20 @@ def download_video_series(video_links, save_path):
 		# obtain filename by splitting url and getting
 		# last string
 		file_name = link.split('/')[-1]
-		file_name = file_name.replace('%20', ' ')
-		file_name = file_name.replace('%27', "'")
+		file_name = file_name.replace(r'%20', r' ')
+		file_name = file_name.replace(r'%27', r"'")
+		file_name = file_name.replace(r'%2b', r'+')
 
+		# file size
+		file_size = int(video_dic[link])
+
+		# download chunk size
+		chunk_size = 1024*1024
+
+		# checking if video file is already present in directory
 		vid_files = os.listdir(save_path)
 		if file_name in vid_files:
-			print( "File:{} already in {}\n".format(file_name,save_path))
+			print( "File:{} already present in {}\n".format(file_name,save_path))
 			continue
 		
 		s_path = os.path.join(save_path,file_name)
@@ -54,13 +84,14 @@ def download_video_series(video_links, save_path):
 		
 		# download started
 		with open(s_path, 'wb') as f:
-			for chunk in r.iter_content(chunk_size = 1024*1024):
+			for chunk in progress.bar(r.iter_content(chunk_size = 1024*1024),expected_size=(file_size/chunk_size) + 1):
 				if chunk:
 					f.write(chunk)
+					f.flush
 		
 		print( "{} downloaded at {}!\n".format(file_name, save_path) )
 
-	print ("All videos downloaded!")
+	print ("All videos of this season downloaded!\n\n")
 	return
 
 
@@ -85,9 +116,9 @@ if __name__ == "__main__":
 		archive_url = "http://51.158.153.210/the-office/season-{}/".format(x)
 
 		# getting all video links
-		video_links = get_video_links(archive_url)
-		print( '\n\n{} {}\n'.format(len(video_links), video_links) )
+		video_dic = get_video_dic(archive_url)
+		print('{}\t{}\n'.format(len(video_dic),video_dic))
 
 		# download all videos
-		download_video_series(video_links, save_path)
+		download_video_series(video_dic, save_path)
 	
